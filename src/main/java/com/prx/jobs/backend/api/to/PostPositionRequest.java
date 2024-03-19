297fd44637af281eeb594a687a75178b4ad63800
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-03-18 21:44:04 -0400

    JB-23 Create position - fix - empty values - Fixed empty values validation. - Created the unit test cases.

--- src/main/java/com/prx/jobs/backend/api/to/PostPositionRequest.java
+++ src/main/java/com/prx/jobs/backend/api/to/PostPositionRequest.java
@@ -1,11 +1,14 @@
 package com.prx.jobs.backend.api.to;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 /**
  * Request object for creating a new position.
  */
-public record PostPositionRequest(@NotNull @NotBlank String name, @NotNull @NotBlank String description,
-                                  @NotNull @NotBlank Boolean active) {
+public record PostPositionRequest(
+        @NotNull(message = "Name value is null, but is required") @NotBlank(message = "Name is blank, but is required") @NotEmpty String name,
+        @NotNull(message = "Description value is null, but is required") @NotBlank(message = "Description value is blank, but is required") @NotEmpty String description,
+        @NotNull(message = "Active is null, but is required") @NotBlank(message = "Active is blank, but is required") @NotEmpty Boolean active) {
 }
