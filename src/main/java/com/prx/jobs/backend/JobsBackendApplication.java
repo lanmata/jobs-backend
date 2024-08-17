1e5a1498fd3445e93bae5f075796e029a3d5335d
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-08-17 07:40:54 -0400

    JB-80 Fix Eureka issue Fails pending to solve for spring boot and spring cloud developers

--- src/main/java/com/prx/jobs/backend/JobsBackendApplication.java
+++ src/main/java/com/prx/jobs/backend/JobsBackendApplication.java
@@ -2,10 +2,12 @@ package com.prx.jobs.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * The main class of the application.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class JobsBackendApplication {
     public static void main(String[] args) {
